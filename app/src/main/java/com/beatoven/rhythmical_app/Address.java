c9066531a260a9c6ed4cf51873f7d740ad3e0dfa
highsare <highsare@naver.com>
2018-05-02 13:07:24 +0900

    xd
    sd

--- app/src/main/java/com/beatoven/rhythmical_app/Address.java
+++ app/src/main/java/com/beatoven/rhythmical_app/Address.java
@@ -10,4 +10,5 @@ public class Address {
     public static final String ADDRESS_SR11_JJ = "http://10.10.11.173:8888/rhythmical/";
     public static final String ADDRESS_SR12_JJ = "http://10.10.12.145:8888/rhythmical/";
     public static final String ADDRESS_SR10_JJ = "http://10.10.10.239:8888/rhythmical/";
+    public static final String ADDRESS_SR09_HJ = "http://203.233.199.83:8888/rhythmical/";
 }
